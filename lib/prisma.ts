3aea7fe55bd651a3718eb74d180e0c1edc229934
Dr. Puma <pnnbao@gmail.com>
2025-08-21 10:33:55 +0700

    fix prisma config

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,12 @@
 import { PrismaClient } from '../lib/generated/prisma'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-// Chỉ khởi tạo, không có logic gì khác
-const prisma = new PrismaClient()
+const globalForPrisma = global as unknown as { 
+    prisma: PrismaClient
+}
+
+const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
